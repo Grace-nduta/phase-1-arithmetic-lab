14dd19bdcbfb521ac986216c3868df4b632b3ecf
Grace Nduta <ndutak5674@gmail.com>
2025-03-04 01:04:03 +0500

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//Define num1 and num2 which will result to a product of 62
+const num1=31
+const num2=2
+const multiply=num1*num2
 
-// Write your code here
+//Generate a random integer greater than 0
+const random=Math.floor(Math.random()*100)+1
+
+//Define num3 and num4 that give a remainder of 4
+const num3=14
+const num4=5
+const mod=num3%num4//mod equal to 4
+
+//Define a set of numbers that gives a max of 20
+const numbers=[10,5,20,15]
+const max=Math.max(...numbers)
